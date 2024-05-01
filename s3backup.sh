1ede2a3782d41af3880869b8526964121fe1d533
franck fesse <me@franckfesse.com>
2024-05-01 11:20:08 +0100

    .gitignore added

--- s3backup.sh
+++ s3backup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 # AUTHOR : Franck Fesse <me@franckfesse.com>
 # Creation date : 22 May 2022
-# Last Edited :  22 May 2022
+# Last Edited :  29 April 2024
 # Description: AWS s3 backup script to be executed to backup your files or directories
 # to your select aws s3 bucket.
 
+# Upload the config
+source .s3-config
+
 # Show how to use this script
-function _s3backup_show_usage() {
-    echo -e "Usage:\n$0 [arguments] \n"
-    echo "Arguments:"
-    echo "--help (-h): Display this help message"
-    echo "--add (-a): Add directory or file to backup"
-    echo "--list (-l): List the directory and file backup and also the s3 bucket"
-    echo "--remove (-r): Remove directory we don't need to sync anymore"
-    exit 0
-}
+help="
+    Usage: $0 [arguments] 
+    Arguments:
+        --help (-h): Display this help message
+        --add (-a): Add directory or file to backup
+        --list (-l): List the directory and file backup and also the s3 bucket
+        --remove (-r): Remove directory we don't need to sync anymore
+"
 
 function backup() {
   echo "Backing up your files now ..."
-  aws s3 ls
+  aws s3 sync $Default_directory s3://$Default_bucket_name/materials
 }
 
 echo "Welcome to s3backup"
-_s3backup_show_usage
+echo "${help}"
 backup
 exit 0
